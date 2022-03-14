7c1efae42189ba8df5bb146684081883a8c1aae9
MBal2 <63755536+MBal2@users.noreply.github.com>
2022-03-13 17:06:52 -0700

    add file manager

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,7 @@ let carObject = function (pData, pType, pYear) {
   this.type = pType;
   this.year = pYear;
 }
-c1 = new carObject("Toyota","SUV",2209);
-c2 = new carObject("Prius","Sedan",2009);
-c3 = new carObject("Kar","Coupe",2019);
-ServercarArray.push(c1);
-ServercarArray.push(c2);
-ServercarArray.push(c3);
-
+ServercarArray = fileManager.read();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -29,7 +23,33 @@ router.get('/getAllData', function(req, res, next) {
 router.post('/AddCar', function(req, res){
   const newCar = req.body;
   ServercarArray.push(newCar);
+  fileManager.write()
   res.status(200);
 });
 
+var fs = require("fs");
+
+fileManager = {
+  read: function(){
+    const stat = fs.statSync('nodesData.json');
+    if(stat.size !==0){
+      var rawdata = fs.readFileSync('nodesData.json');
+      ServercarArray = JSON.parse(rawdata);
+    }
+    else{
+ 
+      c1 = new carObject("Toyota","SUV",2209);
+      c2 = new carObject("Prius","Sedan",2009);
+      c3 = new carObject("Kar","Coupe",2019);
+      ServercarArray.push(c1)
+      ServercarArray.push(c2)
+      ServercarArray.push(c3)
+      fileManager.write();
+    }
+  },
+  write: function(){
+    let data = JSON.stringify(ServercarArray);
+    fs.writeFileSync('nodesData.json', data);
+  }
+}
 module.exports = router;
